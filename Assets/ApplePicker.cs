cb3391447140873464d377d307dd83a047914cf0
zaigiaz <shayne.tman@gmail.com>
2026-02-14 19:52:52 -0600

    mostly working correctly, high score not working correctly

--- Assets/ApplePicker.cs
+++ Assets/ApplePicker.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour
 {
@@ -9,16 +10,35 @@ public class ApplePicker : MonoBehaviour
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
-
+    public List <GameObject> basketlist;
 
     // Start is called before the first frame update
     void Start()
     {
+	basketlist = new List <GameObject>();
         for(int i=0; i<numBaskets; i++) {
 	    GameObject tBasketGo = Instantiate<GameObject>(basketPrefab);
 	    Vector3 pos = Vector3.zero;
 	    pos.y = basketBottomY + (basketSpacingY * i);
 	    tBasketGo.transform.position = pos;
+	    basketlist.Add(tBasketGo);
+	}
+    }
+
+
+    public void AppleDestroyed() {
+	GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
+	foreach(GameObject tGO in tAppleArray) {
+	    Destroy(tGO);
+	}	
+	
+	int basketIndex = basketlist.Count - 1;
+	GameObject tBasketGo = basketlist[basketIndex];
+	basketlist.RemoveAt(basketIndex);
+	Destroy(tBasketGo);
+
+	if(basketlist.Count == 0) {
+	    SceneManager.LoadScene("_Scene_0");
 	}
     }
 }
--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour
 {
+
+    public Text scoreGT;
+
+    void Start() {
+	GameObject scoreGO = GameObject.Find("ScoreCounter");
+	scoreGT = scoreGO.GetComponent<Text>();
+	scoreGT.text = "0";
+	
+    }
 
     // Update is called once per frame
     void Update()
@@ -20,10 +30,20 @@ public class Basket : MonoBehaviour
 
     void OnCollisionEnter(Collision coll) {
 	GameObject collidedWith = coll.gameObject;
+
 	if(collidedWith.tag == "Apple") {
+	    Destroy(collidedWith);
+	    int score = int.Parse( scoreGT.text );
+	    score += 100;
+	    scoreGT.text = score.ToString();		
+
+
+	    if(score > HighScore.score) {
+		HighScore.score = score;
+	    }
+
+	} else if(collidedWith.tag == "Branch") {
 	    Destroy(collidedWith);
 	}
     }
-
-
 }
